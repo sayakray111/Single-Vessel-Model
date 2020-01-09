13eca87161078c4d94719b038076e6cb82463728
sayakray111 <45761455+sayakray111@users.noreply.github.com>
2020-01-10 00:00:02 +1300

    Add files via upload

--- test.py
+++ test.py
@@ -1,31 +1,42 @@
-
 import matplotlib.pyplot as plt 
 import numpy as np
 from decimal import Decimal
 X = []
+Cp = []
+Cx = []
+Cy = []
+Cz = []
 C = []
-C1 = []
-f = open("/hpc/sray036/opencmiss/02-darcy-ell-full/output/STATIC_SOLUTION.part0.exnode",'r')
-#f = open("/hpc/sray036/VirtualPregnancy/repro-examples/uteroplacental/advection-diffusion-models/01-advdiff-cuboid/output/iron/My_Work.part0.exnode",'r')
+#f = open("/hpc/sray036/VirtualPregnancy/repro-examples/uteroplacental/darcy-models/02-darcy-ell-full/output/StaticDarcy.part0.exnode",'r')
+f = open("D:/Projects/repro-examples/uteroplacental/darcy-models/00-darcy-testcase/output/STATIC_SOLUTION.part0.exnode")
+currentline  = 1
 ST = f.readlines()
-print(len(ST))
-for n in range(1,5000):
 
-    x_val = float(ST[14*n+6].strip())
-    y_val = float(ST[14*n+7].strip())
-
-    z_val = float(ST[14*n+8].strip())
-
-    if(y_val == 0.5 and z_val == 0.5):
-        #C.append(float(ST[9*n+9].strip()))
-        C.append(float(ST[14*n+12].strip()))
-
+sum1 = 0.0
+for n in range(1,7000):
+    #x_val = float(ST[9*n+6].strip())
+    x_val = round(float(ST[14*n+6].strip()),2)
+    #y_val = float(ST[9*n+7].strip())
+    y_val = round(float(ST[14*n+7].strip()),2)
+    #z_val = float(ST[9*n+8].strip())
+    z_val = round(float(ST[14*n+8].strip()),2)
+    #print(x_val)
+    if(y_val == 0.8 and z_val == 0.6):
+        C.append(round((float(ST[14*n+11].strip())),2))
         X.append(x_val)
         #C1.append(np.exp(x_val+1))
-        C1.append(x_val*0.25)
+        Cp.append(x_val*y_val*z_val)
+        Cz.append(-x_val*y_val)
+        Cy.append(-x_val*z_val)
+        Cx.append(-z_val*y_val)
 
-#print(X)
+for k in range(0,len(Cz)):
+    error = (C[k]-Cz[k])
+    sum1 = sum1+error**2
+    avg = sum1/len(C)
+    avg = np.sqrt(avg)
+print('Average error is:', avg)
 plt.plot(X,C)
-plt.plot(X,C1)
+plt.plot(X,Cz)
 plt.show()
 f.close()
