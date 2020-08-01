702a8b062f479400b2b17b59a635c2e5c74359ff
SayakRay <sayokroy56@gmail.com>
2020-08-02 10:28:36 +1200

    This is the file

--- test_SVM.py
+++ test_SVM.py
@@ -30,6 +30,83 @@ def read_file(filename):
             continue
     return {'Pressure':Test_Pressure,'Perfusion':Test_Perfusion}
 
+def check_perfusion1():
+    Test_Pressure = []
+    Test_Perfusion = []
+    D1= read_file('./files/perfusion(passive).csv')
+    for l,k in zip(D1['Pressure'],D1['Perfusion']):
+        Test_Pressure.append(l)
+        Test_Perfusion.append(k)
+    D2 = p1.Perfusion_pass()
+    Pressure_in = D2['Pressure']
+    perfusion_norm = D2['Normalised Perfusion']
+    perfusion_norm1 = np.interp(Pressure_in,Test_Pressure,Test_Perfusion).tolist()
+    error = 0.0
+    k1 = 0
+    while(k1<len(perfusion_norm1)):
+        error = error+(abs(perfusion_norm1[k1]-perfusion_norm[k1])/perfusion_norm1[k1])
+        if(perfusion_norm[k1]>3.0):
+            break
+        k1+=1
+    avg = error/len(perfusion_norm1)
+    print('The approximate error is around = ',avg*100)
+    if((avg*100)<=1.5):
+        return True
+    else:
+        return False
+
+def check_perfusion2():
+    Test_Pressure = []
+    Test_Perfusion = []
+    D1= read_file('./files/perfusion(myo).csv')
+    for l,k in zip(D1['Pressure'],D1['Perfusion']):
+        Test_Pressure.append(l)
+        Test_Perfusion.append(k)
+    D2 = p2.Perfusion_myo()
+    Pressure_in = D2['Pressure']
+    perfusion_norm = D2['Normalised Perfusion']
+    perfusion_norm1 = np.interp(Pressure_in,Test_Pressure,Test_Perfusion).tolist()
+    error = 0.0
+    k1 = 0
+    while (k1 < len(perfusion_norm1)):
+        error = error + (abs(perfusion_norm1[k1] - perfusion_norm[k1]) / perfusion_norm1[k1])
+        if (perfusion_norm[k1] > max(Test_Perfusion)):
+            break
+        k1 += 1
+    avg = error / len(perfusion_norm1)
+    print('The approximate error is around = ', avg * 100)
+    if ((avg * 100) <= 1.5):
+        return True
+    else:
+        return False
+
+def check_perfusion3():
+    Test_Pressure = []
+    Test_Perfusion = []
+    D1= read_file('./files/perfusion(myo+shear).csv')
+    for l,k in zip(D1['Pressure'],D1['Perfusion']):
+        Test_Pressure.append(l)
+        Test_Perfusion.append(k)
+    D2 = p3.Perfusion_shear()
+    Pressure_in = []
+    perfusion_norm = []
+    Pressure_in = D2['Pressure']
+    perfusion_norm = D2['Normalised Perfusion']
+    perfusion_norm1 = np.interp(Pressure_in,Test_Pressure,Test_Perfusion).tolist()
+    error = 0.0
+    k1 = 0
+    while (k1 < len(perfusion_norm1)):
+        error = error + (abs(perfusion_norm1[k1] - perfusion_norm[k1]) / perfusion_norm1[k1])
+        if (perfusion_norm[k1] > max(Test_Perfusion)):
+            break
+        k1 += 1
+    avg = error / len(perfusion_norm1)
+    print('The approximate error is around = ', avg * 100)
+    if ((avg * 100) <= 1.5):
+        return True
+    else:
+        return False
+
 def check_perfusion4():
     Test_Pressure = []
     Test_Perfusion = []
@@ -43,12 +120,23 @@ def check_perfusion4():
     Pressure_in = D2['Pressure']
     perfusion_norm = D2['Normalised Perfusion']
     perfusion_norm1 = np.interp(Pressure_in,Test_Pressure,Test_Perfusion).tolist()
-    error = sum(abs(perfusion_norm[i]-perfusion_norm1[i])/perfusion_norm1[i] for i in range(0,len(perfusion_norm1)))
-    error = error/len(perfusion_norm1)
-    print('The approximate error is around = ',error*100)
+    error = 0.0
+    k1 = 0
+    while (k1 < len(perfusion_norm1)):
+        error = error + (abs(perfusion_norm1[k1] - perfusion_norm[k1]) / perfusion_norm1[k1])
+        if (perfusion_norm[k1] > max(Test_Perfusion)):
+            break
+        k1 += 1
+    avg = error / len(perfusion_norm1)
+    print('The approximate error is around = ', avg * 100)
+    if ((avg * 100) <= 1.5):
+        return True
+    else:
+        return False
 
 
-check_perfusion4()
+
+
 
 
 
